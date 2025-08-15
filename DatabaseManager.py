280d3908aa502b162c020b65f006a6e1f5f5e31d
iamak <33217801+i-a-m-a-k@users.noreply.github.com>
2025-08-15 01:55:26 +0100

    Renamed user to user_id coz I kept getting confused :/

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -7,8 +7,8 @@ class DatabaseManager:
 	def __init__(self, db_name: str):
 		self.db = SqliteDict(db_name, autocommit=True)
 
-	def store_text(self, user: str, key: str, value: str, overwrite=False) -> str:
-		user_db = self.db.get(user, {})
+	def store_text(self, user_id: str, key: str, value: str, overwrite=False) -> str:
+		user_db = self.db.get(user_id, {})
 		if key in user_db and not overwrite:
 			match traceback.extract_stack()[1].name: # function which called this
 				case 'add':
@@ -25,35 +25,38 @@ class DatabaseManager:
 					return constants.KEY_EXISTS
 
 		user_db[key] = value
-		self.db[user] = user_db
+		self.db[user_id] = user_db
 		return constants.SUCCESSFUL
 
-	def retrieve_text(self, user: str, key: str) -> Optional[str]:
-		return self.db.get(user, {}).get(key)
+	def retrieve_text(self, user_id: str, key: str) -> Optional[str]:
+		return self.db.get(user_id, {}).get(key)
 
-	def get_user_keys(self, user: str) -> list:
-		return list(self.db.get(user, {}).keys())
+	def get_user_keys(self, user_id: str) -> list:
+		return list(self.db.get(user_id, {}).keys())
 
 	def close(self):
 		self.db.close()
 
-	def delete_key(self, user: str, key: str) -> bool:
-		user_db = self.db.get(user, {})
+	def delete_key(self, user_id: str, key: str) -> bool:
+		user_db = self.db.get(user_id, {})
 		if key in user_db:
 			del user_db[key]
-			self.db[user] = user_db
+			self.db[user_id] = user_db
 			return True
 		return False
 
 	def copy_database(self, new_db:SqliteDict):
+		'''
+		Create a copy of the database
+		'''
 		for key in self.db.keys():
 			new_db[key] = self.db[key]
 
 		return new_db
 
-	def delete_user(self, user:str) -> bool:
+	def delete_user(self, user_id:str) -> bool:
 		try:
-			self.db.pop(user)
+			self.db.pop(user_id)
 			return True
 		except Error as e:
 			print(e)
