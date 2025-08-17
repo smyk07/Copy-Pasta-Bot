f717e6280cac5df59a4bc68f776a55a824249e19
Samyak <samyak.bambole07@gmail.com>
2025-08-17 21:38:13 +0530

    Fix indentation issues in add.py (sorry for late realization)

--- cmds/add.py
+++ cmds/add.py
@@ -4,29 +4,29 @@ from Message import Message
 
 
 def add(message: Message, db: DatabaseManager, overwrite: bool = False) -> str:
-    args = message.args
-    reply = Message(message.reference) if message.reference else None
+	args = message.args
+	reply = Message(message.reference) if message.reference else None
 
-    if len(args) == 1:
-        return constants.WRONG_ARGS_ADD
+	if len(args) == 1:
+		return constants.WRONG_ARGS_ADD
 
-    if len(args) == 2:
-        if reply is None and len(message.message_obj.attachments) == 0:
-            return constants.WRONG_ARGS_ADD
+	if len(args) == 2:
+		if reply is None and len(message.message_obj.attachments) == 0:
+			return constants.WRONG_ARGS_ADD
 
-        original_message = ""
-        if reply:
-            original_message = (getattr(reply, "content", "") or "").strip()
-            original_message += _get_attachments_text(reply)
-        else:
-            original_message = _get_attachments_text(message)
+		original_message = ""
+		if reply:
+			original_message = (getattr(reply, "content", "") or "").strip()
+			original_message += _get_attachments_text(reply)
+		else:
+			original_message = _get_attachments_text(message)
 
-        if not original_message:
-            return constants.EMPTY_MESSAGE
+		if not original_message:
+			return constants.EMPTY_MESSAGE
 
-        return db.store_text(message.author.id, args[1], original_message, overwrite)
+		return db.store_text(message.author.id, args[1], original_message, overwrite)
 
-    return db.store_text(message.author.id, args[1], " ".join(args[2:]), overwrite)
+	return db.store_text(message.author.id, args[1], " ".join(args[2:]), overwrite)
 
 
 def _get_attachments_text(message: Message) -> str:
